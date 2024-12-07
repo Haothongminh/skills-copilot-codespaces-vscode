8bfd3b8236ebb839f0347c258ec8a051468caaa5
Haothongminh <quocthanhvlo2024@gmail.com>
2024-12-07 16:24:44 +0700

    Update

---
+++ hh.py
@@ -0,0 +1,7 @@
+# hack nasa fake
+a= input("bạn hack nasa fake ?")
+b= input("....ơ...")
+for i in range(100):
+   print("hack nasa fake đúng không: ",i,"%done")
+
+print(f"nasa ơi có hack fake kìa Nasa cảm ơn {a} nasa oh..không bùm {b}")
